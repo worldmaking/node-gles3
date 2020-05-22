e2d78518afc3ca283ca20c22a0567f07e1ee3de8
Graham Wakefield <grrrwaaa@gmail.com>
2020-05-22 17:42:58 -0400

    openvr basic working example

--- src/node-openvr.cpp
+++ src/node-openvr.cpp
@@ -5,14 +5,15 @@
 napi_value init(napi_env env, napi_value exports) {
 	napi_status status;
 	napi_property_descriptor properties[] = {
-		{ "vrConnect", 0, vrConnect, 0, 0, 0, napi_default, 0 },
-		{ "vrGetTextureWidth", 0, vrGetTextureWidth, 0, 0, 0, napi_default, 0 },
-		{ "vrGetProjection", 0, vrGetProjection, 0, 0, 0, napi_default, 0 },
-		{ "vrGetTextureHeight", 0, vrGetTextureHeight, 0, 0, 0, napi_default, 0 },
-		{ "vrUpdate", 0, vrUpdate, 0, 0, 0, napi_default, 0 },
-		{ "vrSubmit", 0, vrSubmit, 0, 0, 0, napi_default, 0 }
+		{ "connect", 0, connect, 0, 0, 0, napi_default, 0 },
+		{ "getTextureWidth", 0, getTextureWidth, 0, 0, 0, napi_default, 0 },
+		{ "getTextureHeight", 0, getTextureHeight, 0, 0, 0, napi_default, 0 },
+		{ "getProjection", 0, getProjection, 0, 0, 0, napi_default, 0 },
+		{ "getView", 0, getView, 0, 0, 0, napi_default, 0 },
+		{ "update", 0, update, 0, 0, 0, napi_default, 0 },
+		{ "submit", 0, submit, 0, 0, 0, napi_default, 0 }
 	};
-	status = napi_define_properties(env, exports, 6, properties);
+	status = napi_define_properties(env, exports, 7, properties);
 	//assert(status == napi_ok);
 	return exports;
 }
