45f088f6e3071d4a3ff51e2d9a9d39fa6f318356
Graham Wakefield <grrrwaaa@gmail.com>
2020-06-26 14:03:52 -0400

    mousePick is also mouselook + WASD

--- mousePick.js
+++ mousePick.js
@@ -13,8 +13,6 @@ let updating = true;
 let t = 0
 let fps = 60;
 
-// TODO: mouselook.
-
 let mouse = {
 	// mouse position in homongenous clip coordinates:
 	clip: [0, 0],
@@ -127,10 +125,38 @@ let wasdNav = {
 		let at = vec3.add(vec3.create(), this.pos, this.fwd);
 		return mat4.lookAt(viewmatrix, this.pos, at, this.up);
 	},
-
-	
 };
 
+
+// p0, p1 are the min/max bounding points of the cube
+// rayDir is assumed to be normalized to length 1
+// boxPos, boxQuat, rayOrigin, rayDir are all assumed to be in world space
+function intersectCube(boxPos, boxQuat, p0, p1, rayOrigin, rayDir) {
+	// convert ray origin/direction to object-space:
+	let origin = vec3.sub(vec3.create(), rayOrigin, boxPos);
+	glutils.quat_unrotate(origin, boxQuat, origin);
+	let dir = glutils.quat_unrotate(vec3.create(), boxQuat, rayDir);
+	// using p = origin + dir*t
+	// get ray `t` for each bounding plane of the cube:
+	let t0 = [
+		(p0[0]-origin[0])/dir[0],
+		(p0[1]-origin[1])/dir[1],
+		(p0[2]-origin[2])/dir[2],
+	];
+	let t1 = [
+		(p1[0]-origin[0])/dir[0],
+		(p1[1]-origin[1])/dir[1],
+		(p1[2]-origin[2])/dir[2],
+	];
+	// sort into first (entry) and second (exit) hits:
+	let tmin = vec3.min(vec3.create(), t0, t1); 
+	let tmax = vec3.max(vec3.create(), t0, t1);
+	// ray is a hit if the furthest entry plane is before the nearest exit plane
+	let tentry = Math.max(tmin[0], tmin[1], tmin[2])
+	let texit = Math.min(tmax[0], tmax[1], tmax[2])
+	// hit if entry is before exit:
+	return [tentry <= texit && texit > 0, tentry];
+}
 
 function initGL() {
 	if (!glfw.init()) {
@@ -333,37 +359,6 @@ let window = makeWindow();
 let render = makeRender();
 animate();
 
-// p0, p1 are the min/max bounding points of the cube
-// rayDir is assumed to be normalized to length 1
-// boxPos, boxQuat, rayOrigin, rayDir are all assumed to be in world space
-function intersectCube(boxPos, boxQuat, p0, p1, rayOrigin, rayDir) {
-	// convert ray origin/direction to object-space:
-	let origin = vec3.sub(vec3.create(), rayOrigin, boxPos);
-	glutils.quat_unrotate(origin, boxQuat, origin);
-	let dir = glutils.quat_unrotate(vec3.create(), boxQuat, rayDir);
-
-	// using p = origin + dir*t
-	// get ray `t` for each bounding plane of the cube:
-	let t0 = [
-		(p0[0]-origin[0])/dir[0],
-		(p0[1]-origin[1])/dir[1],
-		(p0[2]-origin[2])/dir[2],
-	];
-	let t1 = [
-		(p1[0]-origin[0])/dir[0],
-		(p1[1]-origin[1])/dir[1],
-		(p1[2]-origin[2])/dir[2],
-	];
-	// sort into first (entry) and second (exit) hits:
-	let tmin = vec3.min(vec3.create(), t0, t1); 
-	let tmax = vec3.max(vec3.create(), t0, t1);
-	// ray is a hit if the furthest entry plane is before the nearest exit plane
-	let tentry = Math.max(tmin[0], tmin[1], tmin[2])
-	let texit = Math.min(tmax[0], tmax[1], tmax[2])
-	// hit if entry is before exit:
-	return [tentry <= texit && texit > 0, tentry];
-}
-
 function animate() {
 	if(glfw.windowShouldClose(window) || glfw.getKey(window, glfw.KEY_ESCAPE)) {
 		shutdown();
@@ -408,6 +403,7 @@ function animate() {
 		hits[0][0].i_highlight[0] = 1;
 	}
 
+	// attribtues may have changed, so resubmit to GPU:
 	cubes.bind().submit()
 
 	gl.viewport(0, 0, dim[0], dim[1]);
