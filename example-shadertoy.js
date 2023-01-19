18da3101c1944b4d795b2ae9ee39740d42a23243
Graham Wakefield <grrrwaaa@gmail.com>
2023-01-19 12:05:27 -0800

    toy

--- example-shadertoy.js
+++ example-shadertoy.js
@@ -10,14 +10,20 @@ let window = new Window()
 let toy = new Shadertoy(gl, {
 	window,
 	//dim: [2048, 2048],
-	common: `shaders/shadertoyCommon.glsl`,
+	// can be GLSL code string or a file path:
+	common: `shaders/shadertoyCommon.glsl`, 
+	// up to 5 shaders:
 	shaders: [
 		{ 
+			// code can be GLSL code string or a file path:
 			code:`shaders/shadertoyA.glsl`,
+			// inputs can refer to a shader pass (integer) or an image file path
 			inputs: [0, "textures/lichen.jpg"]
 		},
 		{ 
+			// code can be GLSL code string or a file path:
 			code:`shaders/shadertoyImage.glsl`,
+			// inputs can refer to a shader pass (integer) or an image file path
 			inputs: [0]
 		},
 	],
